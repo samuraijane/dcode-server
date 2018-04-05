37fba6a3c5b5498b481fa9e06bc16edbedda5292
Matthew Day <mday1@central1.com>
2018-04-05 11:25:26 -0400

    Add environment variables to config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.DATABASE_URL = 'mongodb://localhost/dcode';
+exports.PORT = process.env.PORT || 3001;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
