1087cbc1c5911fde180cfee3d04c2a400b8f45b6
Matthew Day <mday1@central1.com>
2018-04-10 23:58:17 -0400

    Add variable to connect mLab to Heroku

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ exports.DATABASE_URL = 'mongodb://localhost/dcode';
 exports.PORT = process.env.PORT || 3001;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
+exports.MONGODB_URI = process.env.MONGODB_URI;
